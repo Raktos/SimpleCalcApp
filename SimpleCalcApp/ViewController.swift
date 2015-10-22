155e0ccfccf49842dc2afd25bb259adc9defd353
Jason Ho <raktos@uw.edu>
2015-10-22 16:43:51 -0700

    Initial Commit

---
+++ SimpleCalcApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalcApp
+//
+//  Created by iGuest on 10/22/15.
+//  Copyright (c) 2015 Jason Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
