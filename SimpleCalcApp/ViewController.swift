ff50eeb2abb58b7485f02fe89bc8ba6bb4dd652b
Jason Ho <raktos@uw.edu>
2015-10-26 13:30:34 -0700

    basic button presses stubbed

--- SimpleCalcApp/ViewController.swift
+++ SimpleCalcApp/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textBox: UITextField!
+    
+    
+    @IBAction func numberButtonPress(sender: UIButton) {
+        textBox.text = "\(sender.currentTitle!) num pressed"
+    }
+    
+    @IBAction func decimalButtonPress(sender: UIButton) {
+        textBox.text = "\(sender.currentTitle!) decimal pressed"
+    }
+    
+    @IBAction func basicFuncButtonPress(sender: UIButton) {
+        textBox.text = "\(sender.currentTitle!) basic func pressed"
+    }
+    
+    @IBAction func complexFuncButtonPress(sender: UIButton) {
+        textBox.text = "\(sender.currentTitle!) complex func pressed"
+    }
+    
+    @IBAction func rpnButtonPress(sender: UIButton) {
+        textBox.text = "\(sender.currentTitle!) rpn pressed"
+    }
+    
+    
+    @IBAction func equalButtonPress(sender: UIButton) {
+        textBox.text = "\(sender.currentTitle!) equals pressed"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
